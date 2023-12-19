87efb4af01ff3a298f2c62317bec776cc76aebd6
Rivaldy SBS (Semangat Betul Saya) Was Here <muhammadrivaldy16@gmail.com>
2023-12-19 22:09:22 +0700

    Create function to calling order statuses

--- src/stores/Services.js
+++ src/stores/Services.js
@@ -32,3 +32,34 @@ export async function LoginAPI(email, password) {
 
   return result;
 }
+
+export async function GetOrderStatuses(token) {
+  let result = {
+    code: 500,
+    message: 'unexpected error',
+    data: null,
+  };
+
+  await axios
+    .get(baseURL + '/api/v1/orders/statuses', {
+      headers: {
+        Authorization: `Bearer ${token}`,
+      },
+    })
+    .then(response => {
+      if (typeof response.data !== 'undefined') {
+        result.code = response.data.code;
+        result.message = response.data.message;
+        result.data = response.data.data;
+      }
+    })
+    .catch(error => {
+      if (typeof error.response.data !== 'undefined') {
+        result.code = error.response.data.code;
+        result.message = error.response.data.message;
+        result.data = error.response.data.data;
+      }
+    });
+
+  return result;
+}
--- src/stores/Services.test.js
+++ src/stores/Services.test.js
@@ -1,8 +1,16 @@
-import {LoginAPI} from './Services';
+import {GetOrderStatuses, LoginAPI} from './Services';
 
 describe('Login API', () => {
   test('Login Success', async () => {
     var data = await LoginAPI('admin@example.com', 'SayaAdmin');
-    console.log(data.message);
+    console.log(data);
+  });
+});
+
+describe('Get Order Statuses', () => {
+  test('Get Statuses Success', async () => {
+    var resp = await LoginAPI('admin@example.com', 'SayaAdmin');
+    var data = await GetOrderStatuses(resp.data.token);
+    console.log(data);
   });
 });
