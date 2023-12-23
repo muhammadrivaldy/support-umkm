69bd9b8b22c18f3efc8c225efd80164c74e29375
Rivaldy SBS (Semangat Betul Saya) Was Here <muhammadrivaldy16@gmail.com>
2023-12-24 01:28:33 +0700

    Adding toast to reminder the user to fulfill all of the attribute

--- src/components/organism/CreateOrder_AddingService.js
+++ src/components/organism/CreateOrder_AddingService.js
@@ -13,20 +13,27 @@ import {
   TopNavigation,
   TopNavigationAction,
 } from '@ui-kitten/components';
-import {RefreshControl, ScrollView} from 'react-native';
 import {GetLaundryInfo, GetToken} from '../../stores/Storages';
 import {
   GetPackagesByServiceIdAndStoreIdAPI,
   GetServicesByStoreIdAPI,
 } from '../../stores/Services';
 import Toast from 'react-native-toast-message';
+import {
+  CustomPrice,
+  StaticPriceXPieces,
+  StaticPriceXSquareMeter,
+  StaticPriceXWeight,
+} from '../../models/Price_Types';
 
 export function CreateOrder_AddingServiceScreen({navigation}) {
   const [selectedServiceName, setSelectedServiceName] = React.useState(null);
   const [selectedPackage, setSelectedPackage] = React.useState(null);
-  const [refreshing, setRefreshing] = React.useState(false);
   const [services, setServices] = React.useState([]);
   const [packages, setPackages] = React.useState([]);
+  const [priceType, setPriceType] = React.useState(null);
+  const [quantity, setQuantity] = React.useState(0);
+  const [description, setDescription] = React.useState('');
 
   const backIcon = props => <Icon {...props} name="arrow-back" />;
 
@@ -74,7 +81,7 @@ export function CreateOrder_AddingServiceScreen({navigation}) {
               Toast.show({
                 type: 'error',
                 text1:
-                  'Service "' +
+                  'Jasa "' +
                   responseServices.data.service_name +
                   '" belum punya paket',
                 text1Style: {fontFamily: 'Raleway-Bold', fontWeight: '600'},
@@ -98,150 +105,171 @@ export function CreateOrder_AddingServiceScreen({navigation}) {
     });
   };
 
-  const onRefresh = React.useCallback(() => {
-    setRefreshing(true);
-    setTimeout(() => {
-      setRefreshing(false);
-    }, 1500);
-  }, []);
+  const validateBeforeSubmit = () => {
+    if (selectedServiceName !== null) {
+      if (selectedPackage !== null) {
+        if (quantity > 0) {
+          if (description !== '') {
+            return true;
+          }
+        }
+      }
+    }
+
+    return false;
+  };
 
   return (
-    <ScrollView
-      contentContainerStyle={{flexGrow: 1}}
-      refreshControl={
-        <RefreshControl refreshing={refreshing} onRefresh={onRefresh} />
-      }>
-      <Layout style={{flex: 1}}>
-        <TopNavigation
-          title="Tambah Jasa"
-          accessoryLeft={backAction(navigation)}
-          navigation={navigation}
-        />
+    <Layout style={{flex: 1}}>
+      <TopNavigation
+        title="Tambah Jasa"
+        accessoryLeft={backAction(navigation)}
+        navigation={navigation}
+      />
 
-        <Divider />
+      <Divider />
+
+      <Layout style={{marginVertical: 4}} />
+
+      <Layout
+        style={{
+          flex: 1,
+          paddingHorizontal: 8,
+          paddingVertical: 4,
+        }}>
+        <Select
+          placeholder={'Pilih service'}
+          label={TextProps => {
+            TextProps.style[1].fontWeight = '600';
+            return (
+              <Text category="s1" {...TextProps}>
+                Jasa
+              </Text>
+            );
+          }}
+          value={
+            selectedServiceName !== null
+              ? services[selectedServiceName.row].string
+              : null
+          }
+          selectedIndex={selectedServiceName}
+          onSelect={index => setSelectedServiceName(index)}>
+          {services.map(idx => (
+            <SelectItem
+              title={idx.string}
+              key={idx.id}
+              onPressOut={() => {
+                setSelectedPackage(null);
+                setPriceType(idx.price_type);
+                initPackages(idx.id);
+              }}
+            />
+          ))}
+        </Select>
 
         <Layout style={{marginVertical: 4}} />
 
-        <Layout
-          style={{
-            flex: 1,
-            paddingHorizontal: 8,
-            paddingVertical: 4,
-          }}>
-          <Select
-            placeholder={'Pilih service'}
-            label={TextProps => {
-              TextProps.style[1].fontWeight = '600';
-              return (
-                <Text category="s1" {...TextProps}>
-                  Service
-                </Text>
-              );
-            }}
-            value={
-              selectedServiceName !== null
-                ? services[selectedServiceName.row].string
-                : null
-            }
-            selectedIndex={selectedServiceName}
-            onSelect={index => setSelectedServiceName(index)}>
-            {services.map(idx => (
-              <SelectItem
-                title={idx.string}
-                key={idx.id}
-                onPressOut={() => {
-                  setSelectedPackage(null);
-                  initPackages(idx.id);
-                }}
-              />
-            ))}
-          </Select>
-
-          <Layout style={{marginVertical: 4}} />
-
-          <Select
-            placeholder={'Pilih paket nya'}
-            label={TextProps => {
-              TextProps.style[1].fontWeight = '600';
-              return (
-                <Text category="s1" {...TextProps}>
-                  Paket
-                </Text>
-              );
-            }}
-            value={
-              selectedPackage !== null
-                ? packages[selectedPackage.row].name
-                : null
-            }
-            selectedIndex={selectedPackage}
-            onSelect={index => setSelectedPackage(index)}>
-            {packages.map(idx => (
-              <SelectItem title={idx.name} key={idx.id} />
-            ))}
-          </Select>
-
-          <Layout style={{marginVertical: 4}} />
-
-          <Input
-            placeholder="Masukkan ukuran order"
-            inputMode="decimal"
-            label={TextProps => {
-              TextProps.style[0].fontWeight = '600';
-              return (
-                <Text category="s1" {...TextProps}>
-                  Ukuran
-                </Text>
-              );
-            }}
-            accessoryRight={() => {
-              return (
-                <Layout
-                  style={{
-                    borderWidth: 0,
-                    minWidth: 40,
-                    backgroundColor: 'transparent',
-                  }}>
-                  <Text category="s2" style={{textAlign: 'center'}}>
-                    Rp
-                  </Text>
-                </Layout>
-              );
-            }}
-          />
-
-          <Layout style={{marginVertical: 4}} />
-
-          <Input
-            multiline={true}
-            placeholder="Masukkan detail order"
-            textStyle={{minHeight: 100}}
-            label={TextProps => {
-              TextProps.style[0].fontWeight = '600';
-              return (
-                <Text category="s1" {...TextProps}>
-                  Deskripsi
-                </Text>
-              );
-            }}
-          />
-        </Layout>
-
-        <Button
-          style={{
-            marginHorizontal: 8,
-            marginBottom: 8,
+        <Select
+          placeholder={'Pilih paket nya'}
+          label={TextProps => {
+            TextProps.style[1].fontWeight = '600';
+            return (
+              <Text category="s1" {...TextProps}>
+                Paket
+              </Text>
+            );
           }}
-          onPress={() => {
-            navigation.navigate('CreateOrderScreen');
-          }}>
-          {TextProps => {
-            TextProps.style.fontFamily = 'Raleway-Bold';
-            TextProps.style.fontWeight = '600';
-            return <Text {...TextProps}>Tambahkan Jasa</Text>;
+          value={
+            selectedPackage !== null ? packages[selectedPackage.row].name : null
+          }
+          selectedIndex={selectedPackage}
+          onSelect={index => setSelectedPackage(index)}>
+          {packages.map(idx => (
+            <SelectItem title={idx.name} key={idx.id} />
+          ))}
+        </Select>
+
+        <Layout style={{marginVertical: 4}} />
+
+        <Input
+          placeholder="Masukkan ukuran order"
+          inputMode="decimal"
+          onChangeText={text => setQuantity(text)}
+          label={TextProps => {
+            TextProps.style[0].fontWeight = '600';
+            return (
+              <Text category="s1" {...TextProps}>
+                Ukuran
+              </Text>
+            );
           }}
-        </Button>
+          accessoryRight={() => {
+            return (
+              <Layout
+                style={{
+                  borderWidth: 0,
+                  minWidth: 40,
+                  backgroundColor: 'transparent',
+                }}>
+                <Text category="s2" style={{textAlign: 'center'}}>
+                  {priceType === StaticPriceXPieces
+                    ? 'pcs'
+                    : priceType === StaticPriceXWeight
+                    ? 'Kg'
+                    : priceType === CustomPrice
+                    ? 'Rp'
+                    : priceType === StaticPriceXSquareMeter
+                    ? 'M²'
+                    : null}
+                </Text>
+              </Layout>
+            );
+          }}
+        />
+
+        <Layout style={{marginVertical: 4}} />
+
+        <Input
+          multiline={true}
+          placeholder="Masukkan detail order"
+          textStyle={{minHeight: 100}}
+          onChangeText={text => setDescription(text)}
+          label={TextProps => {
+            TextProps.style[0].fontWeight = '600';
+            return (
+              <Text category="s1" {...TextProps}>
+                Deskripsi
+              </Text>
+            );
+          }}
+        />
       </Layout>
-    </ScrollView>
+
+      <Button
+        style={{
+          marginHorizontal: 8,
+          marginBottom: 8,
+        }}
+        onPress={() => {
+          if (!validateBeforeSubmit()) {
+            Toast.show({
+              type: 'error',
+              text1: 'Lengkapi dulu dong 😡',
+              text1Style: {fontFamily: 'Raleway-Bold', fontWeight: '600'},
+              text2: 'Tolong isi jasa, paket, ukuran, dan deskripsi',
+              text2Style: {fontFamily: 'Raleway-Regular'},
+              position: 'bottom',
+            });
+          } else {
+            navigation.goBack();
+          }
+        }}>
+        {TextProps => {
+          TextProps.style.fontFamily = 'Raleway-Bold';
+          TextProps.style.fontWeight = '600';
+          return <Text {...TextProps}>Tambahkan Jasa</Text>;
+        }}
+      </Button>
+    </Layout>
   );
 }
---
+++ src/models/Price_Types.js
@@ -0,0 +1,4 @@
+export const StaticPriceXPieces = 1;
+export const StaticPriceXWeight = 2;
+export const CustomPrice = 3;
+export const StaticPriceXSquareMeter = 4;
