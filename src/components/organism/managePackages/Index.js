1687ec91f07f4354c1d3ace2e8d1463bbdfee131
Rivaldy SBS (Semangat Betul Saya) Was Here <muhammadrivaldy16@gmail.com>
2023-12-31 14:05:39 +0700

    Adding button add package

--- src/components/organism/managePackages/Index.js
+++ src/components/organism/managePackages/Index.js
@@ -1,23 +1,28 @@
 /* eslint-disable react-native/no-inline-styles */
 import React from 'react';
-import {Layout} from '@ui-kitten/components';
+import {Card, Layout} from '@ui-kitten/components';
 import {Header} from './Header';
 import {FormServiceName} from './FormServiceName';
 import {FormServiceType} from './FormServiceType';
+import {ButtonPackage} from './ButtonPacakge';
 
 export function ManagePackagesScreen(props) {
   const mainContent = () => {
     return (
-      <Layout style={{flex: 1, paddingVertical: 4, paddingHorizontal: 8}}>
-        {FormServiceName(props)}
-        <Layout style={{marginVertical: 4}} />
-        {FormServiceType(props)}
+      <Layout style={{flex: 1, paddingVertical: 8, paddingHorizontal: 8}}>
+        <Card status="success" disabled={true}>
+          {FormServiceName(props)}
+          <Layout style={{marginVertical: 4}} />
+          {FormServiceType(props)}
+          <Layout style={{marginVertical: 6}} />
+        </Card>
       </Layout>
     );
   };
 
   return (
     <Layout style={{flex: 1}}>
+      {ButtonPackage()}
       {Header(props)}
       {mainContent()}
     </Layout>
