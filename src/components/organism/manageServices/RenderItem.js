4eaad4a17bad16b945cc55c6e9f94f148f6331a3
Rivaldy SBS (Semangat Betul Saya) Was Here <muhammadrivaldy16@gmail.com>
2023-12-31 17:16:17 +0700

    Adding button delete in package management

--- src/components/organism/manageServices/RenderItem.js
+++ src/components/organism/manageServices/RenderItem.js
@@ -1,3 +1,4 @@
+/* eslint-disable react-native/no-inline-styles */
 /* eslint-disable no-shadow */
 /* eslint-disable react/no-unstable-nested-components */
 import React from 'react';
@@ -12,11 +13,23 @@ export function RenderItem(props) {
       accessoryLeft={<Icon {...props} name="cube-outline" />}
       accessoryRight={() => {
         return (
-          <Layout>
+          <Layout style={{flexDirection: 'row'}}>
             <Button
               size="tiny"
               disabled={item.storeId === 0 ? true : false}
               accessoryLeft={props => <Icon {...props} name="edit-outline" />}
+              onPress={() => {
+                props.navigation.navigate('ManagePackagesScreen');
+              }}
+            />
+
+            <Layout style={{marginHorizontal: 4}} />
+
+            <Button
+              size="tiny"
+              status="danger"
+              disabled={item.storeId === 0 ? true : false}
+              accessoryLeft={props => <Icon {...props} name="trash-outline" />}
               onPress={() => {
                 props.navigation.navigate('ManagePackagesScreen');
               }}
