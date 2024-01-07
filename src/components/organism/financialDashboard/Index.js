4eb5b25c8fd1568a2a5d982e3008ca09e4199ad7
Rivaldy SBS (Semangat Betul Saya) Was Here <muhammadrivaldy16@gmail.com>
2024-01-08 01:09:08 +0700

    Integrationg API financial statistic

--- src/components/organism/financialDashboard/Index.js
+++ src/components/organism/financialDashboard/Index.js
@@ -1,6 +1,7 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable react-native/no-inline-styles */
-import React, {useState} from 'react';
-import {Layout, Tab, TabBar, Text} from '@ui-kitten/components';
+import React, {useEffect, useState} from 'react';
+import {Button, Layout, Select, SelectItem, Text} from '@ui-kitten/components';
 import {Dimensions, ScrollView} from 'react-native';
 import {LineChart} from 'react-native-chart-kit';
 import {GetLaundryInfo, GetToken} from '../../../stores/Storages';
@@ -10,32 +11,41 @@ import {Loading} from '../../molecules/Loading';
 import {FormattingNumberToMoney} from '../../../utils/Currency';
 
 const screenWidth = Dimensions.get('window').width;
-
-var templateData = {
-  labels: [],
-  datasets: [
-    {
-      data: [],
-      color: (opacity = 1) => `rgba(2, 100, 184, ${opacity})`,
-    },
-  ],
-};
+const times = ['years', 'months', 'days'];
+const showTimes = ['Tahunan', 'Bulanan', 'Harian'];
+const years = [2024, 2023, 2022];
+const months = [
+  'Januari',
+  'Februari',
+  'Maret',
+  'April',
+  'Mei',
+  'Juni',
+  'Juli',
+  'Agustus',
+  'September',
+  'Oktober',
+  'November',
+  'Desember',
+];
 
 export function FinancialDashboardScreen(props) {
   const [once, setOnce] = useState(true);
+  const [data, setData] = useState({
+    labels: [],
+    datasets: [
+      {
+        data: [],
+        color: (opacity = 1) => `rgba(2, 100, 184, ${opacity})`,
+      },
+    ],
+  });
   const [loadingVisible, setLoadingVisible] = useState(true);
-  const [data, setData] = useState(Object.assign({}, templateData));
+  const [selectedTime, setSelectedTime] = useState({row: 0});
+  const [selectedYear, setSelectedYear] = useState(null);
+  const [selectedMonth, setSelectedMonth] = useState(null);
 
-  // const testTime = 'years';
-  // const testValue = null;
-
-  // const testTime = 'months';
-  // const testValue = '2023';
-
-  const testTime = 'days';
-  const testValue = '1';
-
-  useState(() => {
+  useEffect(() => {
     if (once) {
       setOnce(false);
       GetToken().then(token => {
@@ -43,14 +53,18 @@ export function FinancialDashboardScreen(props) {
           GetStatisticIncomesByStoreIdAPI(
             token,
             laundryInfo.id,
-            testTime,
-            testValue,
+            times[selectedTime.row],
+            getValue(),
           )
             .then(response => {
+              data.labels = [];
+              data.datasets[0].data = [];
+
               response.data.data.statistic.map(value => {
                 data.labels.push(value.title);
                 data.datasets[0].data.push(String(value.incomes));
               });
+
               setData(data);
               setLoadingVisible(false);
             })
@@ -60,16 +74,80 @@ export function FinancialDashboardScreen(props) {
         });
       });
     }
-  }, []);
+  }, [once]);
+
+  const getValue = () => {
+    let time = times[selectedTime.row];
+
+    if (time === 'years') {
+      return null;
+    } else if (time === 'months') {
+      return years[selectedYear.row];
+    } else if (time === 'days') {
+      return selectedMonth.row + 1;
+    }
+
+    return null;
+  };
 
   const lengthBasedOnTime = time => {
     if (time === 'days') {
-      return 4;
-    } else if (time === 'months') {
       return 5;
+    } else if (time === 'months') {
+      return 4.8;
     } else if (time === 'years') {
-      return 4;
+      return 4.4;
     }
+  };
+
+  const formYear = () => {
+    if (times[selectedTime.row] === 'months') {
+      return (
+        <Select
+          placeholder={'Pilih tahun'}
+          style={{marginBottom: 8}}
+          value={selectedYear !== null ? years[selectedYear.row] : null}
+          onSelect={index => setSelectedYear(index)}>
+          {years.map(value => {
+            return <SelectItem title={value} key={value} />;
+          })}
+        </Select>
+      );
+    }
+
+    return null;
+  };
+
+  const formMonth = () => {
+    if (times[selectedTime.row] === 'days') {
+      return (
+        <Select
+          placeholder={'Pilih bulan'}
+          style={{marginBottom: 8}}
+          value={selectedMonth !== null ? months[selectedMonth.row] : null}
+          onSelect={index => setSelectedMonth(index)}>
+          {months.map(value => {
+            return <SelectItem title={value} key={value} />;
+          })}
+        </Select>
+      );
+    }
+
+    return null;
+  };
+
+  const isButtonDisable = () => {
+    let time = times[selectedTime.row];
+
+    if (time === 'years') {
+      return false;
+    } else if (time === 'months') {
+      return selectedYear === null;
+    } else if (time === 'days') {
+      return selectedMonth === null;
+    }
+
+    return true;
   };
 
   return (
@@ -77,7 +155,35 @@ export function FinancialDashboardScreen(props) {
       {Loading(loadingVisible, setLoadingVisible)}
       {loadingVisible === false ? (
         <>
-          <Layout style={{marginBottom: 10}}>
+          <Layout style={{padding: 20, paddingBottom: 0}}>
+            <Select
+              style={{marginBottom: 8}}
+              value={showTimes[selectedTime.row]}
+              onSelect={index => setSelectedTime(index)}>
+              {showTimes.map(value => {
+                return <SelectItem title={value} key={value} />;
+              })}
+            </Select>
+
+            {formYear()}
+            {formMonth()}
+
+            <Button
+              disabled={isButtonDisable()}
+              status="info"
+              onPress={() => {
+                setLoadingVisible(true);
+                setOnce(true);
+              }}>
+              {TextProps => {
+                TextProps.style.fontFamily = 'Raleway-Bold';
+                TextProps.style.fontWeight = '600';
+                return <Text {...TextProps}>Cari</Text>;
+              }}
+            </Button>
+          </Layout>
+
+          <Layout style={{marginBottom: 50}}>
             <ScrollView
               horizontal={true}
               showsHorizontalScrollIndicator={false}>
@@ -85,7 +191,7 @@ export function FinancialDashboardScreen(props) {
                 data={data}
                 width={
                   (screenWidth * data.labels.length) /
-                  lengthBasedOnTime(testTime)
+                  lengthBasedOnTime(times[selectedTime.row])
                 }
                 height={220}
                 renderDotContent={({x, y, index}) => {
@@ -140,7 +246,7 @@ export function FinancialDashboardScreen(props) {
                 formatYLabel={text => {
                   return 'Rp. ' + FormattingNumberToMoney(text.split('.')[0]);
                 }}
-                style={{marginHorizontal: -20, marginTop: 20}}
+                style={{marginTop: 30, marginRight: 75}}
                 chartConfig={{
                   backgroundGradientFrom: '#ffffff',
                   backgroundGradientTo: '#ffffff',
